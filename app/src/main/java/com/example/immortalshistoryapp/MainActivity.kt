60872c4ee1fb72e6f1b43dea8aa8837b6a86538a
ST10438107 <161326353+ST10438107@users.noreply.github.com>
2024-04-03 16:54:53 +0200

    Declared Variables

--- app/src/main/java/com/example/immortalshistoryapp/MainActivity.kt
+++ app/src/main/java/com/example/immortalshistoryapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.immortalshistoryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Age = findViewById<EditText>(R.id.editTextNumber)
+        val Generate = findViewById<Button>(R.id.button)
+        val Immortal = findViewById<TextView>(R.id.textView2)
+        val Clear = findViewById<Button>(R.id.button2)
+        
+
+
+
     }
 }
